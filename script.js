0184e5faa60f3672559920fbeee5528fd054687c
DylanThomaas <dylanthomasfernandez@gmail.com>
2023-01-31 21:02:03 -0300

    2023/01/31
    css metodologia BEM terminado

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const iconoNav = document.getElementById("iconoNav");
 
 function responsiveMenu(){
     var x = document.getElementById("nav");
-    if(x.className === ""){
+    if(x.className === "header__nav"){
         x.className = "responsive";
     }else{
-        x.className = "";
+        x.className = "header__nav";
     }
 };
 
@@ -17,18 +17,18 @@ iconoNav.addEventListener("click",()=>{
 //funcion que desaparece el menu al seleccionar
 function selection(link){
     var options = document.querySelectorAll('#links a');
-    options[0].className = "";
-    options[1].className = "";
-    options[2].className = "";
-    options[3].className = "";
-    options[4].className = "";
-    link.className = "seleccionado";
+    options[0].className = "header__a";
+    options[1].className = "header__a";
+    options[2].className = "header__a";
+    options[3].className = "header__a";
+    options[4].className = "header__a";
+    link.className = "header__a seleccionado";
 
     var x = document.getElementById("nav");
     if(x.className === "responsive"){
-        x.className = "";
+        x.className = "header__nav";
     }else{
-        x.className = "";
+        x.className = "header__nav";
     }
 };
 
