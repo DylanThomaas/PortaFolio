5ff73bae6d3bd82f35cefbb8fb66e1f567b4b26f
DylanThomaas <dylanthomasfernandez@gmail.com>
2023-02-03 12:29:58 -0300

    mejoras del scroll

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i < animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado - 500 < scrollTop){
+        if(alturaAnimado - 700 < scrollTop){
         animado[i].style.opacity = 1;
         animado[i].classList.add("mostrar__arriba");
         }
