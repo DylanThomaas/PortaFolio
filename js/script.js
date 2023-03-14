e335654d3fbca106c4a0d71f048f3db5e6faa08f
DylaanThomas <dylanthomasfernandez@gmail.com>
2023-03-14 18:01:14 -0300

    2023/03/14

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i < animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado - 650 < scrollTop){
+        if(alturaAnimado - 500 < scrollTop){
         animado[i].style.opacity = 1;
         animado[i].classList.add("mostrar__arriba");
         }
